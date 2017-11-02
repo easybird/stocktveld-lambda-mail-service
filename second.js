401f1d169ff7b962da9df77073542a908ae828b9
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2017-11-02 09:22:56 +0100

    add post request with body

--- second.js
+++ second.js
@@ -1,16 +1,16 @@
 // eslint-disable-next-line import/prefer-default-export
 export const hello = (event, context, cb) => {
-  const p = new Promise((resolve) => {
-    resolve('success');
+  const p = new Promise(resolve => {
+    resolve("success");
   });
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Ecma Script) v1.0! Second module!',
-      input: event,
-    }),
+      message: "Go Serverless Webpack (Ecma Script) v1.0! Second module!",
+      // event: event,
+      // input: event && event.input,
+      body: event && event.body
+    })
   };
-  p
-    .then(() => cb(null, response))
-    .catch(e => cb(e));
+  p.then(() => cb(null, response)).catch(e => cb(e));
 };
